f74843dc3a4636ca5ca50b4e8824bb0448346186
Mr. Mahesh Sunkara <46166792+MAHESH9598@users.noreply.github.com>
2019-01-10 23:20:53 +0530

    Initial commit

---
+++ MutableObjects.py
@@ -0,0 +1,20 @@
+"we can do modifications"
+x=[10,20,30]
+print(x)
+print(type(x))
+print(id(x))
+x[1]=42
+print(x)
+print(type(x))
+print(id(x))
+
+"We can create two different mutable objects with Same data (or) elements"
+
+p=[40,50,60]
+print(p)
+print(type(p))
+print(id(p))
+q=[40,50,60]
+print(q)
+print(type(q))
+print(id(q))
